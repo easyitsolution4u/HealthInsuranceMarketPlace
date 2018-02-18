fbe62e8cfcfaa5ccf55585a9b11193ff6c0161ad
easyitsolution4u <sadhikaria@gmail.com>
2018-02-17 21:41:33 -0500

    Add files via upload

---
+++ UnityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using ProjectDosApplication.Repository;
+using Unity;
+using Unity.AspNet.Mvc;
+
+namespace ProjectDosApplication.Infrastructure
+{
+    public class UnityConfiguration
+    {
+        public static void RegisterComponents()
+        {
+            var container = new UnityContainer();
+            container.RegisterType<IRecipientRepository, ReceipientRepository>();
+            container.RegisterType<IAddressRepository, AddressRepository>();
+            DependencyResolver.SetResolver(new UnityDependencyResolver(container));
+        }
+    }
+}
